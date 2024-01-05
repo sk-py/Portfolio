09755eb9a4d3788287dcdb014f270339e245e8fd
sk-py <77550660+sk-py@users.noreply.github.com>
2024-01-06 00:20:18 +0530

    Fetch request url updated

--- app/lib/requesthandler.js
+++ app/lib/requesthandler.js
@@ -1,5 +1,5 @@
 export const sendMessage = async (data) =>
-  fetch("http://mubashir-shaikh/api/contact", {
+  fetch("https://mubashir-shaikh/api/contact", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
