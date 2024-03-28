d025675c8866d469de3d079d5d5780a325926f74
sk-py <77550660+sk-py@users.noreply.github.com>
2024-03-28 13:21:30 +0530

    Changes

--- app/Resume/page.js
+++ app/Resume/page.js
@@ -1,55 +1,55 @@
-"use client";
-import React from "react";
+// "use client";
+// import React from "react";
 
-const Resume = ({ width, lgWidth }) => {
-  return (
-    <div
-      className={`${lgWidth ? lgWidth : "lg:w-[65%]"}  ${
-        width ? width : "w-[80%]"
-      } mx-auto`}
-    >
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: 0,
-          paddingTop: "141.4286%",
-          paddingBottom: 0,
-          boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
-          marginTop: "1.6em",
-          marginBottom: "0.9em",
-          overflow: "hidden",
-          borderRadius: "8px",
-          willChange: "transform",
-        }}
-      >
-        <iframe
-          loading="lazy"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            top: 0,
-            left: 0,
-            border: "none",
-            padding: 0,
-            margin: 0,
-          }}
-          src="https://www.canva.com/design/DAFz93fvUzY/view?embed"
-          allowFullScreen
-          allow="fullscreen"
-        ></iframe>
-      </div>
-      {/* <a
-        href="https://www.canva.com/design/DAFz93fvUzY/view?utm_content=DAFz93fvUzY&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
-        target="_blank"
-        rel="noopener"
-      >
-        Final Resume
-      </a>
-      by shaikh */}
-    </div>
-  );
-};
+// const Resume = ({ width, lgWidth }) => {
+//   return (
+//     <div
+//       className={`${lgWidth ? lgWidth : "lg:w-[65%]"}  ${
+//         width ? width : "w-[80%]"
+//       } mx-auto`}
+//     >
+//       <div
+//         style={{
+//           position: "relative",
+//           width: "100%",
+//           height: 0,
+//           paddingTop: "141.4286%",
+//           paddingBottom: 0,
+//           boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
+//           marginTop: "1.6em",
+//           marginBottom: "0.9em",
+//           overflow: "hidden",
+//           borderRadius: "8px",
+//           willChange: "transform",
+//         }}
+//       >
+//         <iframe
+//           loading="lazy"
+//           style={{
+//             position: "absolute",
+//             width: "100%",
+//             height: "100%",
+//             top: 0,
+//             left: 0,
+//             border: "none",
+//             padding: 0,
+//             margin: 0,
+//           }}
+//           src="https://www.canva.com/design/DAFz93fvUzY/view?embed"
+//           allowFullScreen
+//           allow="fullscreen"
+//         ></iframe>
+//       </div>
+//       {/* <a
+//         href="https://www.canva.com/design/DAFz93fvUzY/view?utm_content=DAFz93fvUzY&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
+//         target="_blank"
+//         rel="noopener"
+//       >
+//         Final Resume
+//       </a>
+//       by shaikh */}
+//     </div>
+//   );
+// };
 
-export default Resume;
+// export default Resume;
